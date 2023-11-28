1fad4c8210e33ed17a7bc607ff35916c860d7b0d
davidagina <davidagina64@gmail.com>
2023-11-28 15:44:15 +0100

    Updated

--- src/gui/CalculatorUi.java
+++ src/gui/CalculatorUi.java
@@ -1,8 +1,16 @@
 package gui;
 
+import constants.CommonConstants;
+
 import javax.swing.*;
 
 public class CalculatorUi extends JFrame {
 
-
+    public CalculatorUi(){
+        super(CommonConstants.APP_NAME);
+        setSize(CommonConstants.APP_SIZE[0], CommonConstants.APP_SIZE[1]);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
+    }
 }
