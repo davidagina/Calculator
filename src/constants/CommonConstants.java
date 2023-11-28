61246fe7063285b60cc20e88111ca79cdf013160
davidagina <davidagina64@gmail.com>
2023-11-28 16:11:46 +0100

    Updated

--- src/constants/CommonConstants.java
+++ src/constants/CommonConstants.java
@@ -4,4 +4,7 @@ public class CommonConstants {
 
     public static final String APP_NAME = "Calculator";
     public static final int[] APP_SIZE = {400, 600};
+
+    public static final int TEXTFIELD_LENGTH = 10;
+    public static final int TEXTFIELD_FONTSIZE = 36;
 }
